075bbcb2fabec3291263191624849231713da88b
Mehmet Fidan <mehmetfidan2090@gmail.com>
2023-12-20 10:10:13 +0300

    update

---
+++ src/main/java/com/example/kuaforUludag/KuaforUludagApplication.java
@@ -0,0 +1,13 @@
+package com.example.kuaforUludag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuaforUludagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuaforUludagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kuaforUludag/KuaforUludagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kuaforUludag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuaforUludagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
