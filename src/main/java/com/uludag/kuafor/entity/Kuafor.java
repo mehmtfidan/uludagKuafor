f7c48c64f62d176cc77fe3bda1f3d3f727b8624e
Nisa Nur Bayrak <nisanurbayrak01@gmail.com>
2023-12-24 01:38:54 +0300

    Update

--- src/main/java/com/uludag/kuafor/entity/Kuafor.java
+++ src/main/java/com/uludag/kuafor/entity/Kuafor.java
@@ -18,6 +18,7 @@ public class Kuafor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, unique = true)
     private String kullaniciAdi;
     private String sifre;
     private String ad;
