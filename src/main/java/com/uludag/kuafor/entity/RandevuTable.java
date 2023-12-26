d5f998d7ae58332e027682362758c30ee0227b8b
Nisa Nur Bayrak <nisanurbayrak01@gmail.com>
2023-12-26 14:55:41 +0300

    Update

---
+++ src/main/java/com/uludag/kuafor/entity/RandevuTable.java
@@ -0,0 +1,12 @@
+package com.uludag.kuafor.entity;
+
+import java.sql.Date;
+import java.time.LocalTime;
+
+
+public class RandevuTable {
+    private Kuafor kuaforId;
+    private LocalTime baslangic_saati;
+    private LocalTime bitis_saati;
+    private Date izinliGun;
+}
