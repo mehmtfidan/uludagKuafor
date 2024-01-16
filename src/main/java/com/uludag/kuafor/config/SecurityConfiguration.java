53b7fa25ef6e9742b8593ffaa799c19130bf2350
Mehmet Fidan <mehmetfidan2090@gmail.com>
2024-01-16 22:29:00 +0300

    update

--- src/main/java/com/uludag/kuafor/config/SecurityConfiguration.java
+++ src/main/java/com/uludag/kuafor/config/SecurityConfiguration.java
@@ -29,18 +29,18 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf()
-                .disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/api/auth/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                .disable();
+//                .authorizeHttpRequests()
+//                .requestMatchers("/api/auth/**")
+//                .permitAll()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .authenticationProvider(authenticationProvider)
+//                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
